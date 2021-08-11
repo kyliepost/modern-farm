396ac3dfcc69df002b86e26895774fad6ac2a28c
kyliepost <kylie.postlethwaite00@gmail.com>
2021-08-11 11:01:32 -0500

    database for my seeds

---
+++ src/scripts/database.js
@@ -0,0 +1,10 @@
+const database = {
+    seeds: [
+        {id: 1, type:"asparagus", height: 24, output: 4},
+        {id: 2, type:"corn", height: 180, output: 6},
+        {id: 3, type:"potato", height: 32, output: 2},
+        {id: 4, type:"soybean", height: 20, output: 4},
+        {id: 5, type:"sunflower", height: 380, output: 3},
+        {id: 6, type:"wheat", height: 230, output: 6}
+    ]
+}
